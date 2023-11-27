4a943c8eb71a160651604c20980ce872814fbe9f
Eric Chen <eschen@cpp.edu>
2023-11-27 00:52:32 -0800

    improve prompts and add default case

--- src/client.clj
+++ src/client.clj
@@ -1,22 +1,35 @@
-(ns client
-  (:require [clojure.java.io :as io]
-            [clojure.core.async :as async]))
+(ns client)
 
+;; Define server IP address and port
 (def IP-address "localhost")
 (def port 8080)
 
+;; Send a message to the server and get output
 (defn send-command [socket command]
   (let [out (java.io.DataOutputStream. (.getOutputStream socket))
         in  (java.io.BufferedReader. (java.io.InputStreamReader. (.getInputStream socket)))]
-    (println "Input:" command)
+
+    ;; Send message to server
     (.writeBytes out (str command "\n"))
     (.flush out)
+
+    ;; Read the response from the server
     (let [response (.readLine in)]
-      (println "Output:" response))))
+      (println "From server:" response)
+
+      ;; If message is "exit" close the socket connection
+      (when (= "exit" command)
+        (println "Closing socket connection.")
+        (.close socket)))))
 
 (defn -main []
+  ;; Create a socket connection to the server
   (let [socket (java.net.Socket. IP-address port)]
+    (println "Connected to: IP: " IP-address " port: " port)
+    ;; Enter a loop to continuously read user input and send commands to the server
     (while true
+      (print "Enter message: ")
+      (.flush *out*) ;; Explicitly flush the output stream to show the prompt
       (let [user-input (clojure.core/read-line)]
         (when user-input
           (send-command socket user-input))))))
--- src/server.clj
+++ src/server.clj
@@ -3,27 +3,40 @@
            [java.net ServerSocket])
   (:require [clojure.core.async :as async]))
 
+;; Start new lightweight thread to handle client
 (defn handle-client [client-socket]
   (async/go
+    ;; Define input and output reader
     (let [in (BufferedReader. (InputStreamReader. (.getInputStream client-socket)))
           out (PrintWriter. (.getOutputStream client-socket))]
       (loop []
         (let [msg (.readLine in)]
           (when msg
             (println "Received message:" msg)
-            (println "Sending back acknowledgment")
-            (.println out "Message received")
-            (.flush out)
+            (let [[operator & operands] (clojure.string/split msg #" ")
+                  result (cond
+                            (= operator "+") (apply + (map #(Integer. %) operands))
+                            (= operator "-") (apply - (map #(Integer. %) operands))
+                            (= operator "*") (apply * (map #(Integer. %) operands))
+                            (= operator "/") (apply / (map #(Double. %) operands))
+                            :else "Invalid operator. Please enter in format [+,-,*,or /] [num] [num]")]
+              (println "Result:" result)
+              (.println out (str result))
+              (.flush out))
             (recur)))))))
 
 (defn -main []
+  ;; Start server socket
   (let [server-socket (ServerSocket. 8080)]
     (println "Server started, listening on port 8080")
+    ;; Start new thread to listen for client connections
     (async/go
       (while true
         (let [client-socket (.accept server-socket)]
           (println "User connected")
+          ;; Send client connection to be handled by other thread
           (handle-client client-socket)))))
+  ;; Keep program running indefinitely
   (while true))
 
 (-main)
